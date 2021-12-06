72f0471f754e5a2d14b75a9ee88413d3605b5399
Konstantin Nik <34697235+mr-konst-nik@users.noreply.github.com>
2021-12-06 11:14:13 +1000

    Add files via upload

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func pExecution(f []func(c chan string)) {
+
+	lenF := len(f)
+	ch := make(chan string, lenF)
+
+	for _, fnc := range f {
+		go fnc(ch)
+	}
+
+	for i := 0; i < lenF; i++ {
+		msg := <-ch
+		fmt.Println(msg)
+	}
+}
+
+func typeA(c chan string) {
+	time.Sleep(2 * time.Second)
+	c <- "Process A finished"
+}
+
+func typeB(c chan string) {
+	time.Sleep(5 * time.Second)
+	c <- "Process B finished"
+}
+
+func typeC(c chan string) {
+	time.Sleep(7 * time.Second)
+	c <- "Process C finished"
+}
+
+func main() {
+
+	funcArr := []func(c chan string){typeA, typeB, typeC}
+
+	fmt.Println("...Start goroutines...")
+	pExecution(funcArr)
+	fmt.Println("...Finish goroutines...")
+}
